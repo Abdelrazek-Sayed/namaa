ee89c039e16ef8645a8cf02b8f2bd9c556127a4d
AbdelrazekSayed <abdosayed40000@gmail.com>
2024-04-30 17:13:36 +0300

    Transaction Module

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            \Modules\Transactions\Interfaces\UserRepositoryInterface::class,
+            \Modules\Transactions\Repositories\UserRepository::class
+        );
     }
 
     /**
